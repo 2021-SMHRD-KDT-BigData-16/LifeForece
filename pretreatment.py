14d48ce7683902ee1de8b72a80d05a8cc8a4641c
한성규 <127702581+seonggyu9804@users.noreply.github.com>
2023-06-05 16:04:35 +0900

    0605 커밋

--- pretreatment.py
+++ pretreatment.py
@@ -6,10 +6,30 @@ def Pretreatment(test_df):
     import pickle
     import copy
 
-    test_df4 = test_df.copy()
+    test_df3 = test_df.copy()
+    # test 데이터
+    
+    test_df3 = test_df3.replace(-1, np.NaN)
+    
+    first_index = test_df3.drop_duplicates('p_id').index
+    
+    colss=test_df3.columns
+    cols=[]
+    
+    for i in range (2,colss.size):
+        cols.append(colss[i])
+        
+    with open(file='./median_dict.pkl', mode='rb') as f:
+        median_dict=pickle.load(f)
+    
+    for col in cols:
+        for i in first_index:
+            if np.isnan(test_df3.loc[i,col])==True:
+                test_df3.loc[i, col] = median_dict[col]
+        test_df3[col] = test_df3[col].fillna(method='ffill')
     # test 데이터
 
-    test_df4 = test_df4.drop(['rec_time'], axis=1)
+    test_df4 = test_df3.drop(['rec_time'], axis=1)
 
     maxlen = 70
     # zero-padding 함수
@@ -20,7 +40,7 @@ def Pretreatment(test_df):
         col_len = len(df4.columns)
         data = []
         # 기본정보는 zero_padding에 모두 동일하게 적용되어야 함으로 따로 취급한다. 나머지는 모두 0.0으로 채운다.
-        basic_col = 'p_id, age, Gender, ICUtype, male, female'
+        basic_col = 'p_id, age, ICUtype, male, female'
         basic_col_list = basic_col.split(', ')
         basic_col_indexer = df4.columns.get_indexer(basic_col_list)
         basic_col_indexer
@@ -55,19 +75,16 @@ def Pretreatment(test_df):
         minmax = pickle.load(f)
 
     test_df5_1 = test_df5.drop(
-        columns=['p_id', 'age', 'Gender', 'ICUtype', 'male', 'female'])
+        columns=['p_id', 'age', 'ICUtype', 'male', 'female'])
 
     test_df5_2 = minmax.transform(test_df5_1)
     test_df5_2 = pd.DataFrame(test_df5_2, columns=test_df5_1.columns)
     test_df5_2.insert(0, 'p_id', test_df5['p_id'])
     test_df5_2.insert(1, 'age', test_df5['age'])
-    test_df5_2.insert(2, 'Gender', test_df5['Gender'])
     test_df5_2.insert(3, 'ICUtype', test_df5['ICUtype'])
     test_df5_2['male'] = test_df5['male']
     test_df5_2['female'] = test_df5['female']
     test_df5 = test_df5_2
-
-    test_df5.Gender.replace({-1.: 1.0}, inplace=True)
 
     test_df5['ICUtype_1.0'] = 0
     test_df5['ICUtype_2.0'] = 0
